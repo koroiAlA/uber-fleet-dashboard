1597a1293570830d88509092274496e7a00dd811
unknown <dev.koroi.rocio@gmail.com>
2026-08-04 00:30:37 -0500

    Fix: configurar outDir y rootDir en tsconfig para generar carpeta dist

--- server/src/loadData.d.ts
+++
@@ -1,2 +0,0 @@
-import 'dotenv/config';
-//# sourceMappingURL=loadData.d.ts.map
--- server/src/prisma.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-declare const prisma: PrismaClient<{
-    adapter: PrismaNeon;
-}, never, import("@prisma/client/runtime/library").DefaultArgs>;
-export default prisma;
-//# sourceMappingURL=prisma.d.ts.map
